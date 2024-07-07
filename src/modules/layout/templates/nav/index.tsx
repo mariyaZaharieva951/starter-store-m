57b70434c0d53101e38ef769af02f27d138d7970
Mariya Zaharieva <mariqtrunkova@gmail.com>
2024-07-07 13:07:33 +0300

    Update and added images

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -1,3 +1,4 @@
+
 import { Suspense } from "react"
 
 import { listRegions } from "@lib/data"
@@ -5,11 +6,64 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
 
+import { useState } from "react";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
+import {
+  faFacebook,
+  faTwitter,
+  faYoutube,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 export default async function Nav() {
   const regions = await listRegions().then((regions) => regions)
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
+      <div className="flex font-rubik max-w-screen justify-between w-[380px] lg:w-full lg:gap-0">
+          <div className="lg:flex hidden gap-3">
+            <Link href="https://www.facebook.com" passHref>
+              <FontAwesomeIcon
+                icon={faFacebook}
+                
+                className="text-gray text-[20px]"
+              />
+            </Link>
+            <Link href="https://www.twitter.com" passHref>
+              <FontAwesomeIcon
+                icon={faTwitter}
+                // size="20px"
+                className="text-gray"
+              />
+            </Link>
+            <Link href="https://www.youtube.com" passHref>
+              <FontAwesomeIcon
+                icon={faYoutube}
+                // size="20px"
+                className="text-gray"
+              />
+            </Link>
+            <Link href="https://www.instagram.com" passHref>
+              <FontAwesomeIcon
+                icon={faInstagram}
+                // size="20px"
+                className="text-gray"
+              />
+            </Link>
+          </div>
+          <div className="lg:flex justify-between lg:gap-3">
+            <div className="hidden lg:block text-gray lg:text-base text-sm cursor-pointer hover:bg-gray-light rounded">
+              Order tracking
+            </div>
+            <div className="hidden lg:block text-gray lg:text-base text-sm cursor-pointer hover:bg-gray-light rounded">
+              Help
+            </div>
+            <div className="hidden lg:block">
+              {/* <LanguageSwitcher /> */}
+            </div>
+          </div>
+        </div>
       <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
         <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
